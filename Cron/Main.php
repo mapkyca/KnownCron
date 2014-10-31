1d273a32c8862756b7eebcb6efc02edec308ccf1
Marcus Povey <marcus@marcus-povey.co.uk>
2014-10-31 10:23:20 +0000

    Initial plugin outline

---
+++ Cron/Main.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace IdnoPlugins\Cron {
+
+    class Main extends \Idno\Common\Plugin {
+
+	function registerPages() {
+	    \Idno\Core\site()->addPageHandler('/cron/([A-Za-z]+)/?', '\IdnoPlugins\Cron\Pages\Endpoint');
+
+	    \Idno\Core\site()->addPageHandler('admin/cron', '\IdnoPlugins\Cron\Pages\Admin');
+	}
+
+	function generateCode() {
+	    return substr(md5(rand()), 0, 10);
+	}
+
+	function getCode() {
+	    if (!isset(\Idno\Core\site()->config->config['cron']))
+		return false;
+
+	    return \Idno\Core\site()->config->config['cron']['code'];
+	}
+
+	function validateCode($code) {
+	    return ($code == $this->getCode());
+	}
+
+    }
+
+}
